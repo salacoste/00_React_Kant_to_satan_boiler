d25d72823bf0424d1a77296bcc25562d599f34dc
SaLacoste <bad_vano@list.ru>
2019-09-18 21:13:03 +0300

    Comment List component start

--- src/pages/allComments/AllComments.js
+++ src/pages/allComments/AllComments.js
@@ -7,6 +7,24 @@ import {CSSTransition} from 'react-addons-css-transition-group'
 import './styles.css'
 
 class MainPage extends PureComponent {
+
+  componentDidMount() {
+    this.
+    this.props.loadComments()
+  }
+
+  getComments() {
+    console.log('2111', this.props.comments)
+    this.props.comments.map((comment)=> {
+      console.log(comment)
+      return (<li key={comment.id}>
+        <h5>{comment.user}</h5>
+        <p>
+          {comment.text}
+        </p>
+      </li>)
+    })
+  }
   
   render () {
     return (
@@ -24,7 +42,12 @@ class MainPage extends PureComponent {
               transitionEnterTimeout={500}
               transitionLeaveTimeout={300}
             > */}
-              <ArticleList articles = {this.props.articles} comments = {this.props.comments} loadArticles = {this.props.loadArticles} loadComments={this.props.loadComments} />
+              {/* <ArticleList articles = {this.props.articles} comments = {this.props.comments} loadArticles = {this.props.loadArticles} loadComments={this.props.loadComments} /> */}
+              <ul>
+                <p>Lorem, ipsum dolor.</p>
+                {this.getComments()}
+              </ul>
+              
             {/* </CSSTransition> */}
             </Col>
           </Row>
@@ -36,6 +59,8 @@ class MainPage extends PureComponent {
 
 MainPage.propTypes = {
   ListHeader: PropTypes.number,
+  articles: PropTypes.array,
+  comments: PropTypes.array
 
 }
 
